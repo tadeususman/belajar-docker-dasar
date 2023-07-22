84d074d08c1e5cfe113a0c3b91a3e36c8b19e0b1
Tadeus Usman <tadeus.udn@gmail.com>
2023-07-22 21:26:27 +0700

    feat: added notes script

---
+++ docker-inspect.sh
@@ -0,0 +1,9 @@
+# untuk melihat detail informasi dari docker image, container, volume, network
+
+docker image inspect [nama image]
+
+docker container inspect [nama contaner]
+
+docker volume inspect [nama volume]
+
+docker network inspect [nama network]
